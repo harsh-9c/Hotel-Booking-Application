7008c1811b44592c7a07c88d67b76308db2f954c
Harsh jangitwar <harshjangitwar@Harshs-MacBook-Air.local>
2023-03-06 20:30:47 +0530

    added single booking page

---
+++ app/src/PlaceGallery.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+
+export default function PlaceGallery({ place }) {
+  const [showAllPhotos, setShowAllPhotos] = useState(false);
+  if (showAllPhotos) {
+    return (
+      <div className="absolute insert-0 bg-black text-white min-h-screen">
+        <div className="bg-black p-8 grid gap-4">
+          <div>
+            <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
+            <button
+              onClick={() => setShowAllPhotos(false)}
+              className="fixed right-12 top-8 flex gap-2 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+              Close photos
+            </button>
+          </div>
+          {place?.photos?.length > 0 &&
+            place.photos.map((photo) => (
+              <div>
+                <img src={"http://localhost:4000/uploads/" + photo} alt="" />
+              </div>
+            ))}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="relative">
+      <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
+        <div>
+          {place.photos?.[0] && (
+            <div>
+              <img
+                onClick={() => setShowAllPhotos(true)}
+                className="aspect-square cursor-pointer object-cover"
+                src={"http://localhost:4000/uploads/" + place.photos[0]}
+                alt=""
+              />
+            </div>
+          )}
+        </div>
+        <div className="grid">
+          {place.photos?.[1] && (
+            <img
+              onClick={() => setShowAllPhotos(true)}
+              className="aspect-square cursor-pointer object-cover"
+              src={"http://localhost:4000/uploads/" + place.photos[1]}
+              alt=""
+            />
+          )}
+          <div className="overflow-hidden">
+            {place.photos?.[2] && (
+              <img
+                onClick={() => setShowAllPhotos(true)}
+                className="aspect-square cursor-pointer object-cover relative top-2"
+                src={"http://localhost:4000/uploads/" + place.photos[2]}
+                alt=""
+              />
+            )}
+          </div>
+        </div>
+      </div>
+      <button
+        onClick={() => setShowAllPhotos(true)}
+        className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 6a3 3 0 013-3h2.25a3 3 0 013 3v2.25a3 3 0 01-3 3H6a3 3 0 01-3-3V6zm9.75 0a3 3 0 013-3H18a3 3 0 013 3v2.25a3 3 0 01-3 3h-2.25a3 3 0 01-3-3V6zM3 15.75a3 3 0 013-3h2.25a3 3 0 013 3V18a3 3 0 01-3 3H6a3 3 0 01-3-3v-2.25zm9.75 0a3 3 0 013-3H18a3 3 0 013 3V18a3 3 0 01-3 3h-2.25a3 3 0 01-3-3v-2.25z"
+            clipRule="evenodd"
+          />
+        </svg>
+        Show more photos
+      </button>
+    </div>
+  );
+}

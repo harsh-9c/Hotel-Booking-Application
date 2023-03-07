d675d4830f5e8fea009c03a5bb26b559093521fe
Harsh jangitwar <harshjangitwar@Harshs-MacBook-Air.local>
2023-03-07 17:19:53 +0530

    changed styling

--- app/src/Layout.jsx
+++ app/src/Layout.jsx
@@ -3,7 +3,11 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div className="py-4 px-8 flex flex-col min-h-screen">
+    // <div className="py-4 px-8 flex flex-col min-h-screen">
+    //   <Header />
+    //   <Outlet />
+    // </div>
+    <div className="py-4 px-8 flex flex-col min-h-screen max-w-4xl mx-auto">
       <Header />
       <Outlet />
     </div>
--- app/src/PlaceGallery.jsx
+++ app/src/PlaceGallery.jsx
@@ -1,29 +1,129 @@
+// import { useState } from "react";
+
+// export default function PlaceGallery({ place }) {
+//   const [showAllPhotos, setShowAllPhotos] = useState(false);
+//   if (showAllPhotos) {
+//     return (
+//       <div className="absolute insert-0 bg-black text-white min-h-screen">
+//         <div className="bg-black p-8 grid gap-4">
+//           <div>
+//             <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
+//             <button
+//               onClick={() => setShowAllPhotos(false)}
+//               className="fixed right-12 top-8 flex gap-2 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black"
+//             >
+//               <svg
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 fill="none"
+//                 viewBox="0 0 24 24"
+//                 strokeWidth={1.5}
+//                 stroke="currentColor"
+//                 className="w-6 h-6"
+//               >
+//                 <path
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                   d="M6 18L18 6M6 6l12 12"
+//                 />
+//               </svg>
+//               Close photos
+//             </button>
+//           </div>
+//           {place?.photos?.length > 0 &&
+//             place.photos.map((photo) => (
+//               <div>
+//                 <img src={"http://localhost:4000/uploads/" + photo} alt="" />
+//               </div>
+//             ))}
+//         </div>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <div className="relative">
+//       <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
+//         <div>
+//           {place.photos?.[0] && (
+//             <div>
+//               <img
+//                 onClick={() => setShowAllPhotos(true)}
+//                 className="aspect-square cursor-pointer object-cover"
+//                 src={"http://localhost:4000/uploads/" + place.photos[0]}
+//                 alt=""
+//               />
+//             </div>
+//           )}
+//         </div>
+//         <div className="grid">
+//           {place.photos?.[1] && (
+//             <img
+//               onClick={() => setShowAllPhotos(true)}
+//               className="aspect-square cursor-pointer object-cover"
+//               src={"http://localhost:4000/uploads/" + place.photos[1]}
+//               alt=""
+//             />
+//           )}
+//           <div className="overflow-hidden">
+//             {place.photos?.[2] && (
+//               <img
+//                 onClick={() => setShowAllPhotos(true)}
+//                 className="aspect-square cursor-pointer object-cover relative top-2"
+//                 src={"http://localhost:4000/uploads/" + place.photos[2]}
+//                 alt=""
+//               />
+//             )}
+//           </div>
+//         </div>
+//       </div>
+//       <button
+//         onClick={() => setShowAllPhotos(true)}
+//         className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500"
+//       >
+//         <svg
+//           xmlns="http://www.w3.org/2000/svg"
+//           viewBox="0 0 24 24"
+//           fill="currentColor"
+//           className="w-6 h-6"
+//         >
+//           <path
+//             fillRule="evenodd"
+//             d="M3 6a3 3 0 013-3h2.25a3 3 0 013 3v2.25a3 3 0 01-3 3H6a3 3 0 01-3-3V6zm9.75 0a3 3 0 013-3H18a3 3 0 013 3v2.25a3 3 0 01-3 3h-2.25a3 3 0 01-3-3V6zM3 15.75a3 3 0 013-3h2.25a3 3 0 013 3V18a3 3 0 01-3 3H6a3 3 0 01-3-3v-2.25zm9.75 0a3 3 0 013-3H18a3 3 0 013 3V18a3 3 0 01-3 3h-2.25a3 3 0 01-3-3v-2.25z"
+//             clipRule="evenodd"
+//           />
+//         </svg>
+//         Show more photos
+//       </button>
+//     </div>
+//   );
+// }
+
 import { useState } from "react";
+import Image from "./Image.jsx";
 
 export default function PlaceGallery({ place }) {
   const [showAllPhotos, setShowAllPhotos] = useState(false);
+
   if (showAllPhotos) {
     return (
-      <div className="absolute insert-0 bg-black text-white min-h-screen">
+      <div className="absolute inset-0 bg-black text-white min-h-screen">
         <div className="bg-black p-8 grid gap-4">
           <div>
             <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
             <button
               onClick={() => setShowAllPhotos(false)}
-              className="fixed right-12 top-8 flex gap-2 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black"
+              className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
                 viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
+                fill="currentColor"
                 className="w-6 h-6"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
+                  fillRule="evenodd"
+                  d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+                  clipRule="evenodd"
                 />
               </svg>
               Close photos
@@ -32,7 +132,7 @@ export default function PlaceGallery({ place }) {
           {place?.photos?.length > 0 &&
             place.photos.map((photo) => (
               <div>
-                <img src={"http://localhost:4000/uploads/" + photo} alt="" />
+                <Image src={photo} alt="" />
               </div>
             ))}
         </div>
@@ -46,10 +146,10 @@ export default function PlaceGallery({ place }) {
         <div>
           {place.photos?.[0] && (
             <div>
-              <img
+              <Image
                 onClick={() => setShowAllPhotos(true)}
                 className="aspect-square cursor-pointer object-cover"
-                src={"http://localhost:4000/uploads/" + place.photos[0]}
+                src={place.photos[0]}
                 alt=""
               />
             </div>
@@ -57,19 +157,19 @@ export default function PlaceGallery({ place }) {
         </div>
         <div className="grid">
           {place.photos?.[1] && (
-            <img
+            <Image
               onClick={() => setShowAllPhotos(true)}
               className="aspect-square cursor-pointer object-cover"
-              src={"http://localhost:4000/uploads/" + place.photos[1]}
+              src={place.photos[1]}
               alt=""
             />
           )}
           <div className="overflow-hidden">
             {place.photos?.[2] && (
-              <img
+              <Image
                 onClick={() => setShowAllPhotos(true)}
                 className="aspect-square cursor-pointer object-cover relative top-2"
-                src={"http://localhost:4000/uploads/" + place.photos[2]}
+                src={place.photos[2]}
                 alt=""
               />
             )}
